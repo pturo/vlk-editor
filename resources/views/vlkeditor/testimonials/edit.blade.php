6ce76e9df4198229670585d19f8a15bd4e7cf602
pturo <pawel.turon1@gmail.com>
2024-01-03 15:32:16 +0100

    start working on testimonials

--- resources/views/vlkeditor/testimonials/edit.blade.php
+++ resources/views/vlkeditor/testimonials/edit.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <div class="container">
         <div class="head">
-            <h1>Edytuj usługę</h1>
+            <h1>Dodaj opinię</h1>
         </div>
         <p>
-            Tworzenie sekcji jest bardzo proste. Wystarczy nadać sekcji nazwę
-            i dodać elementy, które będą się w niej znajdować.
+            Dodawanie opinii jest bardzo proste. Wystarczy podać imię i nazwisko osoby
+            opiniującej z jej awatarem oraz recenzją.
         </p>
         <div class="add-section">
             @if (Session::has('message'))
@@ -15,35 +15,42 @@
                 <div class="msg-feedback-fail" role="alert">{{ Session::get('error') }}</div>
             @endif
             <div class="form-wrapper">
-                <form action="{{ route() }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('testimonials.update', [$testimonial->id]) }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="form-control">
-                        <label for="name">Nazwa usługi</label>
-                        <input id="name" type="text" name="name" value="{{ old('name', '') }}">
+                        <label for="client_avatar"><i class="bx bx-image"></i> Zdjęcie klienta</label>
+                        <input id="client_avatar" type="file" name="client_avatar">
+                    </div>
+                    @error('client_avatar')
+                        <div class="msg-feedback-fail" role="alert">{{ $message }}</div>
+                    @enderror
+                    <div class="form-control">
+                        <label for="name"><i class="bx bx-pen"></i> Imię i nazwisko</label>
+                        <input id="name" type="text" name="name" value="{{ $testimonial->name }}">
                     </div>
                     @error('name')
                         <div class="msg-feedback-fail" role="alert">{{ $message }}</div>
                     @enderror
                     <div class="form-control">
-                        <label for="thumbnail">Miniatura</label>
-                        <input id="thumbnail" type="file" name="thumbnail">
+                        <label for="occupation"><i class="bx bx-pen"></i> Profesja/Stanowisko</label>
+                        <input id="occupation" type="text" name="name" value="{{ $testimonial->occupation }}">
                     </div>
-                    @error('thumbnail')
+                    @error('occupation')
                         <div class="msg-feedback-fail" role="alert">{{ $message }}</div>
                     @enderror
                     <div class="form-control">
-                        <label for="excerpt">Krótki opis usługi</label>
-                        <textarea name="excerpt" id="excerpt" cols="30" rows="10" value="{{ old('excerpt', '') }}"></textarea>
+                        <label for="review"><i class="bx bx-pen"></i> Treść opinii</label>
+                        <textarea name="review" id="review" cols="30" rows="10">{{ $testimonial->review }}</textarea>
                     </div>
-                    @error('excerpt')
+                    @error('review')
                         <div class="msg-feedback-fail" role="alert">{{ $message }}</div>
                     @enderror
                     <div class="button-control">
-                        <button type="submit">Stwórz sekcję</button>
+                        <button type="submit">Stwórz opinię</button>
                     </div>
                 </form>
                 <div class="button-control">
-                    <span><a href="{{ route() }}">Powrót na poprzednią stronę</a></span>
+                    <span><a href="{{ route('testimonials.index') }}">Powrót na poprzednią stronę</a></span>
                 </div>
             </div>
         </div>
