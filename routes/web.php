4b31b4505f4e172c830b09712710f0340a872899
pturo <pawel.turon1@gmail.com>
2024-01-06 23:30:08 +0100

    Config clear cache

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,8 @@ Auth::routes();
 Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('config:clear');
     $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('view:clear');
     $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('optimize:clear');
     return 'DONE'; //Return anything
 });
